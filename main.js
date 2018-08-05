ec4f0f9012d74a0dc4dd0ad5135a20eca94d6bf6
Christian Bahnweg <cebahnweg@gmail.com>
2018-08-05 18:10:33 -0400

    combines column division creation into single code block.

--- main.js
+++ main.js
@@ -11,19 +11,14 @@ for (i = 0; i < rows; i++){
     sketchpad.appendChild(rowdiv);
     console.log(i);
 }
-for (j= 0; j < columns;j++){
+let rowArray = sketchpad.querySelectorAll(".flex-row");
+rowArray.forEach(rowdiv => {
+    for (j= 0; j < columns;j++){
     const columndiv = document.createElement("div");
     columndiv.style.display = "flex";
     columndiv.classList.add("flex-column");
+    rowdiv.appendChild(columndiv);
     console.log(j);
 }
-let rowArray = sketchpad.querySelectorAll(".flex-row");
-rowArray.forEach(rowdiv => {
-    const columndiv = document.createElement("div");
-    for(k = 0; k < columns;k++){
-        rowdiv.appendChild(columndiv);
-        console.log(k);
-    }
-})
-};
+})};
 ColorChange();
