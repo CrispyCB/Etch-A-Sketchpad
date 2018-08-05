c798825fdcb8f6f222d7b0129ad78431ae8b4dec
Christian Bahnweg <cebahnweg@gmail.com>
2018-08-05 09:46:26 -0400

    changes var to const

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ function ColorChange (){
 let rows = 4;
 let columns = 4;
 for (i = 0; i < rows; i++){
-    var rowdiv = document.createElement("div");
+    const rowdiv = document.createElement("div");
     rowdiv.style.display = "flex";
     rowdiv.classList.add("flex-row");
     sketchpad.appendChild(rowdiv);
     console.log(i);
 }
 for (j= 0; j < columns;j++){
-    var columndiv = document.createElement("div");
+    const columndiv = document.createElement("div");
     columndiv.style.display = "flex";
     columndiv.classList.add("flex-column");
     console.log(j);
