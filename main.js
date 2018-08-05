2a663ca9855151190aa6ea11d384f5a4b9a2a529
Christian Bahnweg <cebahnweg@gmail.com>
2018-08-05 09:34:47 -0400

    adds console logs for debugging.

--- main.js
+++ main.js
@@ -8,16 +8,19 @@ for (i = 0; i < rows; i++){
     rowdiv.style.display = "flex";
     rowdiv.classList.add("flex-row");
     sketchpad.appendChild(rowdiv);
+    console.log(i);
 }
 for (j= 0; j < columns;j++){
     var columndiv = document.createElement("div");
     columndiv.style.display = "flex";
     columndiv.classList.add("flex-column");
+    console.log(j);
 }
 var rowArray = sketchpad.querySelectorAll("flex-row");
 rowArray.forEach(rowdiv => {
     for (k = 0; k < columns; k++) {
         rowdiv.appendChild(columndiv);
+        console.log(k);
     }
 })
 };
